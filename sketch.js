5a66dd3814b8993b1bbf7ce1c76604bf2bf1b415
WHEELNUT790 <ryaanrahman016@gmail.com>
2025-03-22 18:57:55 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,34 +3,35 @@ let classifier;
 let synth;
 
 function setup() {
-	canvas=createCanvas(400, 400);
-	background("yellow");
-	canvas.center();
-	classifier = ml5.imageClassifier("DoodleNet");
-	canvas.mouseReleased(classifyCanvas)
-	synth=window.SpeechSynthesis;
+  canvas=createCanvas(400, 400);
+  background("yellow");
+  canvas.center();
+  classifier = ml5.imageClassifier("DoodleNet");
+  canvas.mouseReleased(classifyCanvas);
+  synth=window.speechSynthesis;
 }
+
 function classifyCanvas(){
-	classifier.classify(canvas,gotResult);
+  classifier.classify(canvas, gotResult);
 }
 
 function gotResult(result){
-	console.log(result);
-	document.getElementById('name').innerHTML=result[0].label;
-	document.getElementById('pro').innerHTML=(result[0].confidence*100).toFixed(2)+"%";
+  console.log(result);
+  document.getElementById('name').innerHTML=result[0].label;
+  document.getElementById('pro').innerHTML=(result[0].confidence*100).toFixed(2)+"%";
 
-	let utterthis=new SpeechSynthesisisUtterance(result[0].label);
-	synth.speak(utterthis);
+  let utterthis=new SpeechSynthesisUtterance(result[0].label);
+  synth.speak(utterthis);
 }
 
-funtion draw () {
-	stroke("blue");
-	strokeWeight(13);
-	if(mouseIsPressed){
-		line(pmouseX,pmouseY,mouseX,mouseY);
-	}
-	stroke("blue");
+function draw() {
+  strokeWeight(13);
+  if(mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
+  stroke("blue");
 }
+
 function clearCanvas(){
-	background("yellow")
+  background("yellow");
 }
